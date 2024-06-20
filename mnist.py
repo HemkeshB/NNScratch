cf0d6dfdfcfdcfba554943e5271f51f7a928afd6
hemkeshb <hemkeshbandi@gmail.com>
2024-06-20 00:49:38 -0700

    new repo

--- mnist.py
+++ mnist.py
@@ -2,42 +2,39 @@ import numpy as np
 from keras._tf_keras.keras.datasets import mnist
 from keras._tf_keras.keras.utils import to_categorical
 
+# Import your custom classes and functions
 from dense import Dense
 from activations import Tanh
 from loss_functions import mse, mse_prime
 from network import train, predict
 
-
+# Preprocess data function
 def preprocess_data(x, y, limit):
-    # reshape and normalize input data
-    x = x.reshape(x.shape[0], 28 * 28, 1)
-    x = x.astype("float32") / 255
-    # encode output which is a number in range [0,9] into a vector of size 10
-    # e.g. number 3 will become [0, 0, 0, 1, 0, 0, 0, 0, 0, 0]
-    y = to_categorical(y)
-    y = y.reshape(y.shape[0], 10, 1)
+    x = x.reshape(x.shape[0], 28 * 28, 1)  # Flatten images and add channel dimension
+    x = x.astype("float32") / 255  # Normalize pixel values
+    y = to_categorical(y)  # One-hot encode labels
+    print(y)
+    y = y.reshape(y.shape[0], 10, 1)  # Reshape to (N, 10, 1) to match the network output format
+    print(y)
     return x[:limit], y[:limit]
 
-
-# load MNIST from server
+# Load MNIST data
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
 x_train, y_train = preprocess_data(x_train, y_train, 1000)
 x_test, y_test = preprocess_data(x_test, y_test, 20)
 
-# neural network
+# Define the neural network
 network = [
-    Dense(28 * 28, 100),
-    Tanh(),
-    Dense(100, 40),
+    Dense(28 * 28, 40),
     Tanh(),
     Dense(40, 10),
     Tanh()
 ]
 
-# train
+# Train the network
 train(network, mse, mse_prime, x_train, y_train, epochs=100, learning_rate=0.1)
 
-# test
+# Test the network
 for x, y in zip(x_test, y_test):
     output = predict(network, x)
     print('pred:', np.argmax(output), '\ttrue:', np.argmax(y))
